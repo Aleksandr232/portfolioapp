9f448accb73b3e0479013dab1406f7baae972e2a
Aleksandr232 <aleksrowing232@gmail.com>
2022-03-18 22:21:14 +0300

    app

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View, ImageBackground, Button, Linking, } from 'react-native';
+import React,{useState} from 'react'
+
+
+
+
+const image={uri:'https://st3.depositphotos.com/1006362/12625/i/600/depositphotos_126254668-stock-photo-garibaldi-lake-in-canada.jpg'}
 
 export default function App() {
+const [open, setOpen]=useState(open);
+
+
+const openurl=()=>{
+  Linking.openURL('https://meportfolio.vercel.app/meportfolio').catch(err => console.error('An error occurred', err));
+}
+
+const openme=()=>{
+  Linking.openURL('https://meportfolio.vercel.app/about').catch(err => console.error('An error occurred', err));
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <ImageBackground source={image} style={styles.im} resizeMode="cover">
+          <View style={styles.button}>
+              <Button title='портфолио' onPress={openurl}  color={'#ff8c00'} />
+          </View>
+          <View style={styles.bume}>
+            <Button title='про меня' color={'#ff8c00'}  onPress={openme} style={styles.button} />
+          </View>
+            
+        </ImageBackground>
     </View>
   );
 }
@@ -17,4 +40,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  im:{
+    flex: 1,
+    justifyContent: "center",
+    width:'100%',
+    height:'100%'
+  },
+  button:{
+    flex:1,
+    width: 150,
+    left: 10,
+    justifyContent: 'space-around',
+    color: '#ff8c00'
+  },
+  bume:{
+    bottom: 400,
+    width: 150,
+    left: 235,
+    justifyContent: 'space-around'
+  }
 });
